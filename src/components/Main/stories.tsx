2bca3fb11fcdcb38d1670a86a6bf5f016b5a93b8
Joao Tomaz <joaootomaz@gmail.com>
2024-02-05 06:49:37 -0300

    install and config storybook

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Main from '.'
+
+export default {
+    title: 'Main',
+    parameters:{
+        layout: 'fullscreen'
+    },
+    component: Main
+
+} as Meta;
+
+export const Default: StoryObj = {}
